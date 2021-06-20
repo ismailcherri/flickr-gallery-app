c7d423bd27a6b46413b6ee6b4b18c281fd192c52
Ismail Cherri <ismailcherri@gmail.com>
2021-06-20 19:22:58 +0200

    Fix app spec

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,6 +3,13 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
+  const intersectionObserverMock = () => ({
+    observe: () => null,
+    unobserve: () => null,
+  });
+  window.IntersectionObserver = jest
+    .fn()
+    .mockImplementation(intersectionObserverMock);
   render(<App />);
   const linkElement = screen.getByText(/Flicker Gallery/i);
   expect(linkElement).toBeInTheDocument();
